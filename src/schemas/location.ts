8f68b6253606cfe707fd52691c2b06ebfeae2593
Crypt212 <ahmed.elesseily.d@gmail.com>
2026-04-24 21:11:05 +0300

    make isHidden property optional in location

--- src/schemas/location.ts
+++ src/schemas/location.ts
@@ -15,7 +15,7 @@ export const CreateLocationSchema = z.object({
   cityId: UUIDSchema,
   lat: LatitudeSchema,
   long: LongitudeSchema,
-  isHidden: z.boolean(),
+  isHidden: z.boolean().optional(),
   isMain: z.boolean(),
 });
 
