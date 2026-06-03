11713b8afc4af32b50a87dd5194911e6682f5084
Crypt212 <ahmed.elesseily.d@gmail.com>
2026-06-03 09:15:16 +0300

    fix create route for reports

--- src/services/ReportService.ts
+++ src/services/ReportService.ts
@@ -22,15 +22,15 @@ export default class ReportService extends Service {
 
   async createReport(params: {
     report: ReportCreateInput;
-    requesterRole: string;
+    requesterType: "WORKER" | "CLIENT";
     files?: Express.Multer.File[];
   }): Promise<ReportWithImages> {
     return tryCatch(async () => {
-      if (params.report.targetType === "CLIENT_PROFILE" && params.requesterRole !== 'WORKER') {
+      if (params.report.targetType === "CLIENT_PROFILE" && params.requesterType !== 'WORKER') {
         throw new AppError('Only workers can report client profiles', 403);
       }
 
-      if (params.report.targetType === "WORKER_PROFILE" && params.requesterRole !== 'CLIENT') {
+      if (params.report.targetType === "WORKER_PROFILE" && params.requesterType !== 'CLIENT') {
         throw new AppError('Only clients can report worker profiles', 403);
       }
 
