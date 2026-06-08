a596002e555f8c3070a26cd387b6a44fd99823dd
Crypt212 <ahmed.elesseily.d@gmail.com>
2026-06-09 01:11:17 +0300

    fix access middlewares

--- src/middlewares/accessMiddleware.ts
+++ src/middlewares/accessMiddleware.ts
@@ -5,7 +5,7 @@ import { asyncHandler } from '../types/asyncHandler.js';
  * Disallows client access to the route
  */
 export const unAuthorizeClient = asyncHandler(async (req, _, next) => {
-  if (req.userState.role !== 'CLIENT')
+  if (req.userState.role === 'CLIENT')
     return next(new AppError('Unauthorized access for client users', 403));
 
   next();
@@ -15,8 +15,11 @@ export const unAuthorizeClient = asyncHandler(async (req, _, next) => {
  * Allows only client access to the route
  */
 export const authorizeClient = asyncHandler(async (req, _, next) => {
-  if (req.userState.role === 'CLIENT')
-    return next(new AppError('Unauthorized access for non-client users', 403));
+  if (req.userState.role !== 'CLIENT')
+    return next(new AppError('Only clients are allowed', 403));
+
+  if (!req.userState.client)
+    return next(new AppError('Only users with client profile are allowed', 403));
 
   next();
 });
@@ -37,7 +40,10 @@ export const unAuthorizeWorker = asyncHandler(async (req, _, next) => {
  */
 export const authorizeWorker = asyncHandler(async (req, _, next) => {
   if (req.userState.role !== 'WORKER')
-    return next(new AppError('Unauthorized access for non-worker users', 403));
+    return next(new AppError('Only workers are allowed', 403));
+
+  if (!req.userState.worker)
+    return next(new AppError('Only users with worker profile are allowed', 403));
 
   next();
 });
@@ -47,9 +53,13 @@ export const authorizeWorker = asyncHandler(async (req, _, next) => {
  */
 export const authorizeApprovedWorker = asyncHandler(async (req, _, next) => {
   if (req.userState.role !== 'WORKER')
-    return next(new AppError('Unauthorized access for non-worker users', 403));
+    return next(new AppError('Only workers are allowed', 403));
+
+  if (!req.userState.worker)
+    return next(new AppError('Only users with worker profile are allowed', 403));
+
   if (req.userState.worker.verification.status !== 'APPROVED')
-    return next(new AppError('You are not approved yet', 403));
+    return next(new AppError('You are not an approved worker yet', 403));
 
   next();
 });
